2d6e0cb898895e1e086965bc6434e61c4d8d3fe1
DjaziraOC <djaziranait14@gmil.com>
2024-02-17 21:54:19 +0100

    controller-remplacer create par addClients

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Contracts\Service\Attribute\Required;
+//  importer le model de client
+use App\Models\client;
 
 class ClientsController extends Controller
 {
@@ -17,7 +20,7 @@ class ClientsController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function addClients()
     {
         return view('client.addClient');
     }
@@ -25,40 +28,57 @@ class ClientsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function addclient_traitement(Request $request)
     {
-        //
+        // Validation des données entrées par l'utilisateur
+        $request->validate([
+            'nom' => 'required',
+            'ville' => 'required',
+            'telephone' => 'required',
+        ]);
+
+        // Création d'une nouvelle instance de Client
+        $client = new Client();
+        $client->nom = $request->nom;
+        $client->ville = $request->ville;
+        $client->telephone = $request->telephone;
+
+        // Sauvegarde du client dans la base de données
+        $client->save();
+
+        // Redirection vers la page d'ajout de clients avec un message de succès
+        return redirect('/addClients')->with('status', 'Le client a été ajouté avec succès');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+    // public function show(string $id)
+    // {
+    //     //
+    // }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  */
+    // public function edit(string $id)
+    // {
+    //     //
+    // }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+    // /**
+    //  * Update the specified resource in storage.
+    //  */
+    // public function update(Request $request, string $id)
+    // {
+    //     //
+    // }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    // /**
+    //  * Remove the specified resource from storage.
+    //  */
+    // public function destroy(string $id)
+    // {
+    //     //
+    // }
 }
