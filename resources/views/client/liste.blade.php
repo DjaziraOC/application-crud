abc7f1857b81fa88da8667c79f833ac8a6457c92
DjaziraOC <djaziranait14@gmil.com>
2024-02-17 21:55:18 +0100

    rajouter icon addclient -interface liste

--- resources/views/client/liste.blade.php
+++ resources/views/client/liste.blade.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CRUD IN LARAVEL 10</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <!-- <style>
       .custom-bg:nth-child(odd) {
         background-color:gray; /* Couleur de fond pour les colonnes impaires */
       }
       .custom-bg:nth-child(even) {
         background-color:darkcyan; /* Couleur de fond pour les colonnes paires */
       }
-    </style>
+    </style> -->
   </head>
   <body>
     <div class="container text-center">
@@ -21,7 +22,11 @@
           <hr>
             <h1 class="mt-5">CRUD IN LARAVEL 10</h1>
           <hr>
-            <a href="/addClients" class="btn btn-primary" style="background-color:darkcyan;float:right;margin-bottom:20px;">Ajouter un client</a>
+            <a href="/addClients" class="btn btn-primary" style="background-color:darkcyan;float:right;margin-bottom:20px;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-plus-fill" viewBox="0 0 16 16">
+                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0"/>
+              </svg>
+            </a>
             <table class="table  table-bordered ">
               <thead>
                 <tr>
@@ -100,5 +105,6 @@
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
   </body>
 </html>
