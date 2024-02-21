e4b63b03a31873d074f43844358f076fdbd887a6
DjaziraOC <djaziranait14@gmil.com>
2024-02-21 23:56:34 +0100

    la route :get pour supprimer un client de la liste globale

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,27 @@ use App\Http\Controllers\ClientsController;
 | Route::get('/client', [ClientsController::class, 'methodName']);
 */
 
-// La première route définit une méthode GET pour afficher le formulaire de modification du client.
+// La route 1:définit une méthode GET pour afficher le formulaire de modification du client.
 // Elle pointe vers la méthode edit du contrôleur ClientsController. 
 Route::get('/updateClient/{id}',[ClientsController::class,'edit']);
-// La deuxième route définit une méthode POST pour traiter la soumission du formulaire de modification du client. 
+
+// La route 2:définit une méthode POST pour traiter la soumission du formulaire de modification du client. 
 // Elle pointe vers la méthode updateClient du contrôleur ClientsController.
-Route::post('/updateClient/traitement', [ClientsController::class, 'updateClient']);
+Route::post('/updateClient/traitement',[ClientsController::class,'updateClient']);
+
+// La route 3:définit une méthode GET pour afficher les client sauvgarder dans la base de donnée.
+// Elle pointe vers la méthode liste_clients du contrôleur ClientsController. 
 Route::get('/client',[ClientsController::class,'liste_clients']);
+
+// La route 4:définit une méthode GET pour afficher le formulaire de l'ajout du client.
+// Elle pointe vers la méthode addClients du contrôleur ClientsController.
 Route::get('/addClients',[ClientsController::class,'addClients']);
-Route::post('/addClients/traitement', [ClientsController::class,'addclient_traitement']);
 
+// La route 5:définit une méthode POST pour traiter la soumission du formulaire de l'ajout du client. 
+// Elle pointe vers la méthodeaddclient_traitement du contrôleur ClientsController.
+Route::post('/addClients/traitement',[ClientsController::class,'addclient_traitement']);
 
+// La route 6:définit une méthode GET pour supprimer un client de la liste globale des clients.
+// Elle pointe vers la méthode deleteClient du contrôleur ClientsController. 
+Route::get('/deleteClient/{id}',[ClientsController::class,'deleteClient']);
 
-
-
-
-
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
