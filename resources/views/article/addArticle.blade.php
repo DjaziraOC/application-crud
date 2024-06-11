53afd54ea6a50454386dba96057948bbd514c10d
DjaziraOC <djaziranait14@gmil.com>
2024-06-12 00:21:55 +0200

    navbar et footer modifié

--- resources/views/article/addArticle.blade.php
+++ resources/views/article/addArticle.blade.php
@@ -7,14 +7,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
   </head>
+  <!-- Responsive navbar-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container px-5">
+      <a class="navbar-brand" href="/displayArticles"> HOT TAKES</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">          
+              <li class="nav-item"><a class="nav-link" href="/displayArticles">SAUCES</a></li>
+              <li class="nav-item"><a class="nav-link" href="/">LOGOUT</a></li>
+          </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Page Content-->
   <body>
-    <div class="container">
+    <div class="container" style="min-height: 100vh;">
       <div class="row">
-        <div class="col">
           <hr>
             <h1 class="mt-5">Ajouter un article</h1>
-     
-         <hr>
+          <hr>
             <form class="row  mb-3"  action="/addArticle" method="POST" enctype="multipart/form-data">
               @csrf
               @method('POST')
@@ -44,25 +56,31 @@
                 </div>
                 <div class="row mb-3">
                     <label for="image" class="col-sm-2 col-form-label" >Image</label>
-                    <div class="col-sm-7">
+                    <div class="col-sm-7" text-center>
                       <input type="file" class="form-control" id="image" name="image" accept="image/*" required>
                   </div>
                 <div class="row mb-3">
                     <label for="heat" class="col-sm-2 col-form-label">Piquants</label>
-                    <div class="col-sm-7">
-                        <input type="range" class="form-range"  min="0" max="10" id="heat" name="heat">
+                    <div class="col-sm-7" style="margin-top:40px;">
+                        <input type="range" class="form-range"  min="1" max="10" id="heat" name="heat">
                     </div>
                 </div>
                 <div class="col-12"> 
                     <button type="submit" class="btn btn-primary">Ajouter</button>
-                    <a href="/" type="submit" class="btn btn-danger">Annuler</a> 
+                    <a href="/displayArticles" type="submit" class="btn btn-danger">Annuler</a> 
                 </div>
             </form>
           <hr>
-        </div>
       </div>
     </div>
+    </div>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container px-4 px-lg-5">
+          <p class="m-0 text-center text-white">Copyright &copy; Your Website 2024</p>
+        </div>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="{{ asset('js/bootstrap.js') }}"></script>
-  </body>
+</body>
 </html>
