61f02e1c76c6019caacc893892af6e8cc1fcb744
DjaziraOC <djaziranait14@gmil.com>
2024-06-12 01:22:08 +0200

    ClientsController-fonction showUploadForm

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use Symfony\Contracts\Service\Attribute\Required;
 //  importer le model de client
@@ -84,8 +84,72 @@ class ClientsController extends Controller
         $client->delete();
 
         // Redirection vers la page client avec un message de succès
-        return redirect('/client')->with('status','Les données du client séléctionné ont été supprimé à jour avec succès');
-
+        // return redirect('/client')->with('status','Les données du client séléctionné ont été supprimé à jour avec succès');
     }
 
+    public function showUploadForm()  
+    { 
+        return view('client.uploadImage');
+    }
+
+    // public function  uploadImageStore (Request $request)
+    // {
+    //     // Valider les données du formulaire
+    //     $request->validate([
+    //         'image'=>'required|image|max:3048',
+    //     ]);
+        
+    //     if ($request->hasFile('image')){
+
+            // $path = $request->file('image')->store('images', 'public');
+
+            // // Retourner le chemin de l'image stockée
+            // return view('client.showImage', ['image' => asset('storage/'. $path)]);
+            
+    //         // Stocker l'image dans le système de fichiers
+    //         $picturePath = $request->file('image')->store('images','public');
+
+    //         // Récupérer l'URL de l'image
+    //         $url = Storage::url($picturePath);
+
+    //         // Rediriger vers la vue d'affichage de l'image avec l'URL de l'image
+    //         return redirect()->route('showImage', ['url' => $url]);
+           
+
+    //         // Redirection vers la page client avec un message de succès
+    //         // return redirect('/client')->with('status','l\'image télécharger avec succès');
+    //     }else{
+    //         // Redirection vers la page client avec un message d'erreur
+    //         return redirect()->back()->with('error', 'Veuillez sélectionner une image à télécharger.');
+    //     }
+    // }
+    // public function  showImage ($url){
+    //     return view('client.showImage', ['image'=>$url]);
+    // }
+
+     /**
+     * Store a newly created resource in storage.
+     */
+  
+    // public function pictureStore (Request $request)
+    // {
+    //     $request->validate([
+    //         'image'=>'required|image|max:3048',
+    //     ]);
+
+    //     // Récupérer la liste des fichiers d'images dans le répertoire 'public/images'
+    //     $files = Storage::files('public/images');
+
+    //     // Transformer les chemins des fichiers en URLs
+    //     $imageUrls = array_map(function ($file) {
+    //         return asset(Storage::url($file));
+    //     }, $files);
+
+    //     // Passer les URLs des images à la vue
+    //     return view('article.showArticles', ['image' => $imageUrls]);
+    // }
+    
+
+
+    
 }
